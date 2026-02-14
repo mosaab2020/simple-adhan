4a1231cd4df84e90cece9e2d00185e89ff4d6c3b
mosaab2020 <mosaabomar2020@gmail.com>
2026-02-14 04:52:07 +0400

    update remove aladhan lib

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import aladhan
+import adhanapi
 import json
 import argparse
 
@@ -10,38 +10,33 @@ def main():
     parser.add_argument("-j", "--json", action="store_true", help="output json")
     args = parser.parse_args()
 
-    try:
-        # Ex:
-        # location = aladhan.City("London", "GB")
-        location = aladhan.City("", "")
-        client = aladhan.Client(location)
-        adhans = client.get_today_times()
-    except Exception as e:
-        print("Can't get time:", e)
-        exit(1)
+    # Example: (change the country and city name)
+    prayer_client = adhanapi.PrayClient(country="UK", city="London")
+
+    adhans = prayer_client.today_adhan_times()
 
     tooltip_list = []
     flag_c = ""
 
     for adhan in adhans:
-        if adhan.is_passed() is True:
+        if adhan.time_passed() is True:
             flag_c = "✔"
         else:
             flag_c = " "
 
-        tooltip_list.append("%s %s at %s" % (flag_c, adhan.get_en_name(), adhan.readable_timing(show_date=False)))
+        tooltip_list.append("%s %s at %s" % (flag_c, adhan.s_name, adhan.format_twelve_hour()))
 
     next_adhan = None
 
     for adhan in adhans:
-        if adhan.is_passed() is False:
+        if adhan.time_passed() is False:
             next_adhan = adhan
             break
 
     if next_adhan is None:
         text = "------"
     else:
-        text = "%s %s" % (next_adhan.get_en_name(), next_adhan.readable_timing(show_date=False))
+        text = "%s %s" % (next_adhan.s_name, next_adhan.format_twelve_hour())
 
     tooltip = "\n".join(tooltip_list)
 
