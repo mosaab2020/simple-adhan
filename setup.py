a3d72bb14719e30cf99f5a2cd4c311899b83573f
mosaab2020 <mosaabomar2020@gmail.com>
2026-03-27 15:15:56 +0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="simple-adhan",
-    version="0.3",
+    version="0.4",
     packages=find_packages(),
     install_requires=[
         "requests",
